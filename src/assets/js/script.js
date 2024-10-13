5fff03c4e4885ec97e0a07ab435c575dcce17731
vladlent <plafon89@gmail.com>
2024-10-13 14:34:57 +0300

    commit

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,17 +2,20 @@
 	'use strict';
 
 	const inputThickness = $('input[name="thickness"]'),
-		inputDiameter = $('input[name="diameter"]'),
+		selectDiameter = $('select[name="diameter"]'),
 		length = $('input[name="length"]');
 
 	function rivet_calculation() {
-		$('input[type=number]').on('keyup mouseup', function () {
-			const getThickness = parseFloat($(this).filter('[name=thickness]').val() ? $(this).filter('[name=thickness]').val() : inputThickness.val()),
-				getDiameter = parseFloat($(this).filter('[name=diameter]').val() ? $(this).filter('[name=diameter]').val() : inputDiameter.val()),
-				value = getThickness + getDiameter * 1.2;
-			length.val((Math.trunc(value * 100) / 100));
-		}).trigger('keyup');
+		const getThickness = parseInt(inputThickness.val()),
+			getDiameter = parseFloat(selectDiameter.val()),
+			value = getThickness + (getDiameter * 1.2);
+		length.val(Math.round(value));
 	}
+
+	inputThickness.add(selectDiameter).on('keyup mouseup change', function () {
+		rivet_calculation();
+	}).trigger('keyup');
+
 	rivet_calculation();
 
 }(jQuery));
